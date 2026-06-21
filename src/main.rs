9c8a662192ec808072e4abf53618d96eaccef348
Artur Peixoto <arturokfya@gmail.com>
2026-06-20 22:27:50 -0300

    stuff

--- src/main.rs
+++ src/main.rs
@@ -24,7 +24,7 @@ fn calculate_escape_time(c: Complex<f32>, limit: EscapeLimit) -> Option<EscapeLi
 }
 
 
-fn make_lerp(input: &(f32, f32), output: &(f32, f32)) -> impl Fn(f32) -> f32 {
+fn make_lerp(input: (f32, f32), output: (f32, f32)) -> impl Fn(f32) -> f32 {
     let a = (output.1 - output.0) / (input.1 - input.0);
     let b =  output.0 - (input.0 * a);
     move |x| x*a + b
@@ -58,8 +58,8 @@ fn make_calculations(resolution: Resolution, rect: Rect<f32>, limit: EscapeLimit
         )
         .flatten()
         .map({
-            let x_lerp = make_lerp(&(0_f32, resolution.width as f32), &(x_min, x_max));
-            let y_lerp = make_lerp(&( resolution.height as f32, 0_f32), &(y_min, y_max));
+            let x_lerp = make_lerp((0_f32, resolution.width as f32), (x_min, x_max));
+            let y_lerp = make_lerp(( resolution.height as f32, 0_f32), (y_min, y_max));
 
             move |(x, y)| {
                 let x_c = x_lerp(x as f32);
@@ -134,42 +134,41 @@ fn main(){
 
     let rect_lin_num = 128;
 
-    let x_rect_lerp = make_lerp(&(0.0, rect_lin_num as f32), &(-2.0, 1.0) );
-    let y_rect_lerp = make_lerp(&(0.0, rect_lin_num as f32), &(-1.5, 1.5) );
+    let x_rect_lerp = make_lerp((0.0, rect_lin_num as f32), (-2.0, 1.0) );
+    let y_rect_lerp = make_lerp((0.0, rect_lin_num as f32), (-1.5, 1.5) );
 
     let atlas_squares =
         (0..rect_lin_num)
-        .map(move |x| (0..rect_lin_num).map(move|y|(x, y)))
+        .into_par_iter()
+        .map(move |x| (0..rect_lin_num).into_par_iter().map(move|y|(x, y)))
         .flatten()
-        .par_bridge()
         .map(|(x_i, y_i)|{
-                let rect = {
-                       let x_range = Range::<f32>{min: x_rect_lerp(x_i as f32) , max: x_rect_lerp((x_i + 1) as
-                       f32)};
-                       let y_range = Range::<f32>{min: y_rect_lerp(y_i as f32) , max: y_rect_lerp((y_i + 1) as
-                       f32)};
+            let rect = {
+                    let x_range = Range::<f32>{min: x_rect_lerp(x_i as f32) , max: x_rect_lerp((x_i + 1) as
+                    f32)};
+                    let y_range = Range::<f32>{min: y_rect_lerp(y_i as f32) , max: y_rect_lerp((y_i + 1) as
+                    f32)};
 
-                       Rect::<f32>{x: x_range,y: y_range}
-                };
+                    Rect::<f32>{x: x_range,y: y_range}
+            };
 
-                let string_end = format!("[{:02.3},{:02.3}]_[{:02.3},{:02.3}]", &rect.x.min, &rect.x
-                .max, &rect.y.min, &rect.y.max);
+            let string_end = format!("[{:02.3},{:02.3}]_[{:02.3},{:02.3}]", &rect.x.min, &rect.x
+            .max, &rect.y.min, &rect.y.max);
 
-                let file_name = format!("atlas/mandelbrot_{}", &string_end);
-                let mut image = Image::new(&RESOLUTION);
-                let calculations = make_calculations(RESOLUTION, rect, LIMIT);
-                println!("Starting calculations for {}", &string_end);
-                write_data(&mut image, calculations, LIMIT);
+            let file_name = format!("atlas/mandelbrot_{}", &string_end);
+            let mut image = Image::new(&RESOLUTION);
+            let calculations = make_calculations(RESOLUTION, rect, LIMIT);
+            println!("Starting calculations for {}", &string_end);
+            write_data(&mut image, calculations, LIMIT);
 
-                if (image.data.iter().max().unwrap() - image.data.iter().min().unwrap()) > 20 {
-                    println!("Writing file for {}", &string_end );
-                    save_image(&image, &file_name).unwrap();
-                } else {
-                    println!("Skipping {}", &string_end);
+            if (image.data.iter().max().unwrap() - image.data.iter().min().unwrap()) > 20 {
+                println!("Writing file for {}", &string_end );
+                save_image(&image, &file_name).unwrap();
+            } else {
+                println!("Skipping {}", &string_end);
 
-                }
             }
-        );
+        });
 
 
     let _: Vec<()> = atlas_squares.collect();
